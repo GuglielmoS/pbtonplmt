03f0e53020078e82017d00ea3c17ba261a60fada
Guglielmo Fachini <guglielmo@fachini.net>
2017-03-27 14:37:34 +0200

    change unbound sltc main so that it only searches for progress and preservation counterexamples

--- stlc-unbound/src/Main.hs
+++ stlc-unbound/src/Main.hs
@@ -10,31 +10,5 @@ main = do
   putStrLn ""
 
   putStrLn "Preservation of red"
-  featCheck 13 prop_preservation
+  featCheck 7 prop_preservation
   putStrLn ""
-
-  putStrLn "Confluence of red"
-  featCheck 13 prop_confluenceOfRed
-  putStrLn ""
-
-  putStrLn "algeq is an equivalence relation"
-  putStrLn "reflexivity"
-  featCheck 13 prop_isReflAlgeq
-  putStrLn "symmetry"
-  featCheck 13 prop_isSymAlgeq
-  putStrLn "transitivity"
-  featCheck 13 prop_isTransAlgeq
-  putStrLn ""
-
-  putStrLn "patheq is an equivalence relation"
-  --putStrLn "reflexivity"
-  --featCheck 13 prop_isReflPatheq
-  putStrLn "symmetry"
-  featCheck 13 prop_isSymPatheq
-  putStrLn "transitivity"
-  featCheck 13 prop_isTransPatheq
-  putStrLn ""
-
-  --putStrLn "algeq is equivalent to redcomp"
-  --featCheck 13 prop_algeqEquivRedcomp
-  --putStrLn ""
